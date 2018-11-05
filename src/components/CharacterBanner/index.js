305e252e3040425cb98d75940aefb137383193bb
Maianne Ribeiro <mai628r@gmail.com>
2018-11-04 23:17:32 -0200

    feat(CharacterBanner): add basic header structure

--- src/components/CharacterBanner/index.js
+++ src/components/CharacterBanner/index.js
@@ -1,8 +1,34 @@
-import React, { Component } from 'react';
-import { characterBanner } from './index.css';
+import React, { Component, Fragment } from 'react';
+import {
+  characterBanner,
+  charClass,
+  leftColumn,
+  name,
+  playerName,
+  race,
+  rightColumn,
+  topRow,
+} from './index.css';
 
 export default class CharacterBanner extends Component {
+  renderHeader() {
+    return (
+      <Fragment>
+        <div className={topRow}>
+          <div className={leftColumn}>
+            <span className={name}>Character Name</span>
+            <span className={playerName}>Player Name</span>
+          </div>
+          <div className={rightColumn}>
+            <span className={race}>Character Race</span>
+            <span className={charClass}>Character Class</span>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+
   render() {
-    return <div className={characterBanner}>This is a character banner.</div>;
+    return <div className={characterBanner}>{this.renderHeader()}</div>;
   }
 }

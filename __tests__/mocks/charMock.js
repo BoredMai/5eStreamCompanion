70a85075e1197b93e995e75ac8bf4ff958ac2f49
Maianne Ribeiro <mai628r@gmail.com>
2018-12-26 16:54:09 -0200

    test(app): use charMock on tests

---
+++ __tests__/mocks/charMock.js
@@ -0,0 +1,43 @@
+const PlayerOne = {
+  name: 'Character One',
+  shortName: 'One',
+  player: 'Player One',
+  race: 'Race',
+  classes: [{ name: 'Class', level: 1 }],
+  level: 1,
+  hp: { current: 10, max: 10 },
+  xp: 0,
+  attributes: [
+    { attribute: 'STR', value: 10 },
+    { attribute: 'DEX', value: 10 },
+    { attribute: 'CON', value: 10 },
+    { attribute: 'INT', value: 10 },
+    { attribute: 'WIS', value: 10 },
+    { attribute: 'CHA', value: 10 },
+  ],
+  ac: 16,
+};
+
+const PlayerTwo = {
+  name: 'Character Two',
+  shortName: 'Two',
+  player: 'Player Two',
+  race: 'Race',
+  classes: [{ name: 'Class', level: 20 }],
+  level: 20,
+  hp: { current: 150, max: 150 },
+  xp: 999999,
+  attributes: [
+    { attribute: 'STR', value: 20 },
+    { attribute: 'DEX', value: 20 },
+    { attribute: 'CON', value: 20 },
+    { attribute: 'INT', value: 20 },
+    { attribute: 'WIS', value: 20 },
+    { attribute: 'CHA', value: 20 },
+  ],
+  ac: 26,
+};
+
+const Characters = [PlayerOne, PlayerTwo];
+
+export default Characters;
--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__tests__/mocks/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
+    '\\./assets/characters$': '<rootDir>/__tests__/mocks/charMock.js',
   },
   setupTestFrameworkScriptFile: '<rootDir>__tests__/setupEnzyme.js',
   testMatch: ['**/src/components/**/*.spec.js'],
